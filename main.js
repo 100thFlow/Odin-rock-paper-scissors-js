447f4426a3b3082f8d515600a6c81f672e533c10
100thFlow <100thFlow@proton.me>
2025-02-25 13:08:02 +0200

    Init npm for prettier

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
-function getComputerChoice(){
-  let choice = Math.floor((Math.random() * 100))
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 100);
 
   if (choice < 30) {
-    choice = "paper";
+    choice = 'paper';
   } else if (choice <= 60) {
-    choice = "rock";
+    choice = 'rock';
   } else {
-    choice = "scissors";
+    choice = 'scissors';
   }
 
   return choice;
 }
 
-function getHumanChoice(){
-  let choice = prompt('Type one of "rock", "paper", or "scissors"').toLowerCase()
+function getHumanChoice() {
+  let choice = prompt('Type one of "rock", "paper", or "scissors"').toLowerCase();
 
-  if (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
-   alert('You can type only one of "rock", "paper", or "scissors"') 
+  if (choice !== 'rock' && choice !== 'paper' && choice !== 'scissors') {
+    alert('You can type only one of "rock", "paper", or "scissors"');
     return getHumanChoice();
   } else {
     return choice;
   }
 }
-
